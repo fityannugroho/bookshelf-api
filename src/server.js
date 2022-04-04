3ec28a5f15279cbe1cc2c0a8e91d4e4f83e23b9b
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-04 17:44:15 +0700

    server init

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+(async () => {
+  // Initialize the server.
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: true,
+    },
+  });
+
+  // Add the routes.
+  server.route(routes);
+
+  // Start the server.
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
+})();
