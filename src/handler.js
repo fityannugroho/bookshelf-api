6dfcdafb07668d2c32cbfc39230dd8b5526ebd6d
Fityandhiya Islam Nugroho <fityannugroho@gmail.com>
2022-04-04 20:22:18 +0700

    finish add new book endpoint

--- src/handler.js
+++ src/handler.js
@@ -1,1 +1,74 @@
+const {nanoid} = require('nanoid');
 const books = require('./books');
+
+const addBookHandler = (request, h) => {
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  // Validate input.
+  if (!name) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    }).code(400);
+  }
+
+  if (readPage > pageCount) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage '
+        + 'tidak boleh lebih besar dari pageCount',
+    }).code(400);
+  }
+
+  // Init book.
+  const id = nanoid(16);
+  const finished = pageCount === readPage;
+  const insertedAt = new Date().toISOString();
+  const updatedAt = insertedAt;
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+    finished,
+    insertedAt,
+    updatedAt,
+  };
+
+  // Add book.
+  books.push(newBook);
+
+  // Check if book was added.
+  const isSuccess = books.filter((book) => book.id === id).length === 1;
+  if (isSuccess) {
+    return h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id,
+      },
+    }).code(201);
+  }
+
+  // If failed to add book.
+  return h.response({
+    status: 'error',
+    message: 'Buku gagal ditambahkan',
+  });
+};
+
+module.exports = {addBookHandler};
